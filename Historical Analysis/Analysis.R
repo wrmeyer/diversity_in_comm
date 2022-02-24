8fe10b4f471e5f981432d7b5f4b80badef97a098
wrmeyer <56975717+wrmeyer@users.noreply.github.com>
2022-02-24 13:28:45 -0500

    Writing graph skeleton

--- Historical Analysis/Analysis.R
+++ Historical Analysis/Analysis.R
@@ -3,7 +3,12 @@ library(tidyverse)
 df <- read.csv("/Data_by_year.csv", encoding="UTF-8")
 view(df)
 
-df %>%
-  ggplot(., aes(Year, pctHispanic)) + 
-  geom_point() + 
-  geom_smooth(method=lm)
+p1 <- df %>%
+  ggplot(., aes(Year, pctHispanic)) + geom_point() + geom_smooth() +
+  geom_point(data = df, aes(Year, pctWhite), color = "red") + geom_smooth(data = df, aes(Year, pctWhite), color = "red") +
+  geom_point(data = df, aes(Year, pctBlack), color = "green") + geom_smooth(data = df, aes(Year, pctBlack), color = "green") +
+  geom_point(data = df, aes(Year, pctNatAm), color = "orange") + geom_smooth(data = df, aes(Year, pctNatAm), color = "orange") +
+  geom_point(data = df, aes(Year, pctBiracial), color = "pink") + geom_smooth(data = df, aes(Year, pctBiracial), color = "pink") +
+  geom_point(data = df, aes(Year, pctAsian.PI), color = "black") + geom_smooth(data = df, aes(Year, pctAsian.PI), color = "black")
+  
+  p1
